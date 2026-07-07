6f2a56ffe7c882d55d6ed5afa8375107d5f2e05c
PabloSantos <psantos-2023251@kinal.edu.gt>
2026-07-07 11:24:37 -0600

    feat: Se creo el archivo data con fs/promises de producto

---
+++ Proyecto/src/data/productos.ts
@@ -0,0 +1,22 @@
+import { readFile, writeFile } from 'fs/promises';
+import { Producto } from '../models/producto.js';
+
+const FILE_PATH = './productos.json';
+
+export async function leerProductos(): Promise<Producto[]> {
+  try {
+    const data = await readFile(FILE_PATH, 'utf-8');
+    return JSON.parse(data);
+  } catch (error: any) {
+    if (error.code === 'ENOENT') return [];
+    throw new Error(`Error al leer productos: ${error.message}`);
+  }
+}
+
+export async function guardarProductos(productos: Producto[]): Promise<void> {
+  try {
+    await writeFile(FILE_PATH, JSON.stringify(productos, null, 2));
+  } catch (error: any) {
+    throw new Error(`Error al escribir productos: ${error.message}`);
+  }
+}

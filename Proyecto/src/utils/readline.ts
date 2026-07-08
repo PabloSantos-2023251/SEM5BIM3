244a017cb4fd53cee55a98d44dd642b7494d12dc
PabloSantos <psantos-2023251@kinal.edu.gt>
2026-07-08 07:18:49 -0600

    refractor: se actualizo el readline

--- Proyecto/src/utils/readline.ts
+++ Proyecto/src/utils/readline.ts
@@ -1,6 +1,12 @@
-import * as readline from "readline"
+import * as readline from "readline";
 
 export const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
-})
+});
+
+export function cuestionario(pregunta: string): Promise<string> {
+  return new Promise((resolve) => {
+    rl.question(pregunta, (respuesta) => resolve(respuesta));
+  });
+}

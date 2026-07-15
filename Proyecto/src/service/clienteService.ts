983eac3d4a594a7f1b8aa2d4ba3b20989e273547
PabloSantos-2023251 <psantos-2023251@kinal.edu.gt>
2026-07-15 10:31:01 -0600

    refractor: Se actualizo el service de clientes

--- Proyecto/src/service/clienteService.ts
+++ Proyecto/src/service/clienteService.ts
@@ -1,17 +1,37 @@
-import { leerClientes, guardarClientes } from '../data/clientes.js';
-import { Cliente } from '../models/cliente.js';
+import { ClienteRepository } from "../data/clienteRepository.js";
+import { APICliente } from "../client/cliente.js";
+import { Cliente } from "../models/cliente.js";
 
 export class ClienteService {
-  async registrar(cliente: Cliente): Promise<void> {
-    if (!cliente.nombre || !cliente.nit) {
-      throw new Error('Validación: El cliente requiere nombre y NIT.');
-    }
-    const lista = await leerClientes();
-    lista.push(cliente);
-    await guardarClientes(lista);
+  private repo = new ClienteRepository();
+  private clienteAPI = new APICliente();
+
+  async obtenerClientes(): Promise<Cliente[]> {
+    return await this.repo.leer();
   }
 
-  async obtenerTodos(): Promise<Cliente[]> {
-    return await leerClientes();
+  async sincronizarYGuardar(): Promise<{ clientes: Cliente[]; tiempoTotalMs: string }> {
+    const inicioProceso = performance.now();
+
+    try {
+      const clientesExternos = await this.clienteAPI.obtenerClientesExternos();
+
+      const inicioEscritura = performance.now();
+      await this.repo.guardar(clientesExternos);
+      const finEscritura = performance.now();
+      console.log(`[Archivo] Datos guardados localmente en: ${(finEscritura - inicioEscritura).toFixed(2)} ms`);
+
+      const finProceso = performance.now();
+      const tiempoTotal = (finProceso - inicioProceso).toFixed(2);
+
+      return {
+        clientes: clientesExternos,
+        tiempoTotalMs: tiempoTotal
+      };
+
+    } catch (error: any) {
+      console.error("\n❌ [Servicio] Ocurrió un error al sincronizar los datos.");
+      throw error;
+    }
   }
 }
